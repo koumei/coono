e035574b3120abb747287bed929fffc1758cebea
koumei <koumei@sigmapps.com.au>
2014-03-02 00:27:02 +1100

    Add REST server support
    add codeigniter-restserver API
    (https://github.com/philsturgeon/codeigniter-restserver)

---
+++ web/application/controllers/CoonoAPI.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: koumei
+ * Date: 2/03/14
+ * Time: 12:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class CoonoAPI extends REST_Controller
+{
+
+}
